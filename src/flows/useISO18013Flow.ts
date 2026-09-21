e73f32fcce00aac40968fba21257446bed2bbc87
dhanushrajcredenceid <dhanush.raj@credenceid.com>
2026-09-21 15:26:31 +0530

    Revert "debug: deliberately pass dcRequest.deviceRequest instead of full dcRequest"
    This reverts commit 20f96822730fdbce78a3c4e17117734bcaea638c.

--- src/flows/useISO18013Flow.ts
+++ src/flows/useISO18013Flow.ts
@@ -26,7 +26,7 @@ export function useISO18013Flow() {
     try {
       credential = (await navigator.credentials.get({
         digital: {
-          requests: [{ protocol: "org-iso-mdoc", data: dcRequest.deviceRequest }],
+          requests: [{ protocol: "org-iso-mdoc", data: dcRequest }],
         },
         mediation: "required",
       })) as DigitalCredential | null;
